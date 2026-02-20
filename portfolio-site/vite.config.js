ce2d79950db6038257dbf5e86efbefa292e10b50
Pratik Korat <korat.pratik1@gmail.com>
2026-02-19 23:21:38 -0800

    Update Vite configuration to set base path for portfolio site

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
+  base: '/portfolio-site/',
   plugins: [react(), tailwindcss()],
 })
